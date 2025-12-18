0970bef2eac6006ea6e911ec4a546393c8fca1f1
J. Brandon Kigen <126805003+fka-kafka@users.noreply.github.com>
2025-12-18 09:30:11 +0300

    Fix syntax error in ALLOWED_HOSTS assignment

--- server/config/settings.py
+++ server/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [config('FQDNS']
+ALLOWED_HOSTS = [config('FQDNS')]
 
 
 # Application definition
